7c11c5bb757c89220a202194a0a71b2ca80ab54f
Kollo David <kdavid77@gmail.com>
2014-03-02 10:37:49 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imdb::Application.config.secret_key_base = '08d46dfdc056f68e247060190be2c9dc20856da8c12646de61492869c8874f7982340c3c81dc82c677e9c51671c2542e76c447681ef18367ba1ac907646b34a1'
