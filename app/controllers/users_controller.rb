5c2db2c3bacf61706958859bb3b778bef7bd0101
Kollo David <kdavid77@gmail.com>
2014-03-03 21:03:01 +0200

    signup bugfix usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
+    u=User.all
+    if u.empty?
+      newDefaultUser=User.new(name:"admin", email:"admin@example.com", password:"admin123", password_confirmation:"admin123", admin:true)
+      newDefaultUser.save
+    end
   end
 end
