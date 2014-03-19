d83ab4510114e432e5105c3d0ac9aaa22092db52
Kollo David <kdavid77@gmail.com>
2014-03-19 18:02:52 +0200

    Cleanup finish

--- app/controllers/data_files_controller.rb
+++ app/controllers/data_files_controller.rb
@@ -1,14 +1,11 @@
 class DataFilesController < ApplicationController
-  def uploadFile
-  end
-  
-  def new
-  end
-  
-  def save
+  def savedoc
     @dir="public/docs/" + @real_estate.rs_type
     @path=File.join(@dir, @real_estate.id.to_s + "_" + @df.file_name)
-    if File.open(@path, "wb") { |f| f.write(params[:data_file][:Upload].read) }
+    if File.exist?(@path)
+      flash[:error]="Hiba, a dokumentum már létezik!"
+    else
+      File.open(@path, "wb") { |f| f.write(params[:data_file][:Upload].read) }
       flash[:success]=@df.file_name + " feltöltve!"
       @df.save!
     end
@@ -23,7 +20,7 @@ class DataFilesController < ApplicationController
       @df.file_comment=params[:data_file][:file_comment]
       @df.file_name=params[:data_file][:Upload].original_filename
       @df.user_id=current_user.id
-      self.save
+      self.savedoc
     end
     redirect_to real_estate_path(params[:data_file][:id])
   end
